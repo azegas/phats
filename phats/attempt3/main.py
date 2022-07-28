3046ece504268d8195953b4fa2f09271b2a1702d
arvydasg <azegaspa@gmail.com>
2022-07-28 19:35:31 +0300

    feat: import functions as a whole

--- phats/attempt3/main.py
+++ phats/attempt3/main.py
@@ -1,8 +1,8 @@
-from functions import delete_cols_rows, convert_to_txt, no_sca, merge, istrink_bruksniukus, delete_trash
+import functions
 
-delete_cols_rows()
-no_sca()
-merge()
-convert_to_txt()
-istrink_bruksniukus()
-delete_trash()
+functions.delete_cols_rows()
+functions.no_sca()
+functions.merge()
+functions.convert_to_txt()
+functions.istrink_bruksniukus()
+functions.delete_trash()
