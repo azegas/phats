ea2fd7190562d52ee25efc30d355a33de80da720
arvydasg <azegaspa@gmail.com>
2022-07-29 07:05:13 +0300

    feat: taking user input for the functions. all in one file for now

--- phats/attempt3/functions.py
+++ phats/attempt3/functions.py
@@ -2,33 +2,31 @@ from openpyxl import Workbook, load_workbook
 import pandas as pd
 import os
 
+skaiciukas = int(input("ivesk skaiciuka"))
 
 def delete_cols_rows():
-    wb = load_workbook('sourcefiles/3043210.xlsx')
+    wb = load_workbook('sourcefiles/' + str(skaiciukas)+".xlsx")
     ws = wb['Sheet1']
     # print_rows()
     ws.delete_cols(1, 1)
     ws.delete_rows(1, 5)
     # print_rows()
 
-    wb.save('output/3043210_s1_delete.xlsx')
-
+    wb.save('output/' + str(skaiciukas)+".xlsx")
 
 def no_sca():
-    df = pd.read_excel('output/3043210_s1_delete.xlsx', sheet_name='Sheet1')
+    df = pd.read_excel('output/' + str(skaiciukas)+".xlsx", sheet_name='Sheet1')
     # print(df)
     # Replace sub string
     df = df.replace('SCA', '', regex=True)
     # print(df)
-    datatoexcel = pd.ExcelWriter('output/3043210_s2_noSCA.xlsx')
+    datatoexcel = pd.ExcelWriter('output/' + str(skaiciukas)+".xlsx")
     df.to_excel(datatoexcel)
     datatoexcel.save()
 
-    os.remove('output/3043210_s1_delete.xlsx')
-
 
 def merge():
-    wb = load_workbook('output/3043210_s2_noSCA.xlsx')
+    wb = load_workbook('output/' + str(skaiciukas)+".xlsx")
     ws = wb['Sheet1']
     # print_rows()
 
@@ -36,52 +34,53 @@ def merge():
     ws.delete_rows(1, 1)
     # print_rows()
 
-    wb.save('output/3043210_s3_merge.xlsx')
-    
-    os.remove('output/3043210_s2_noSCA.xlsx')
+    wb.save('output/' + str(skaiciukas)+".xlsx")
 
 
 def convert_to_txt():
     # exportas to .txt
     # https://www.youtube.com/watch?v=M5b669dAm2g
-    xl = pd.ExcelFile('output/3043210_s3_merge.xlsx')
+    xl = pd.ExcelFile('output/' + str(skaiciukas)+".xlsx")
     xl.sheet_names
 
     for sheet in xl.sheet_names:
         file = pd.read_excel(xl, sheet_name=sheet)
-        file.to_csv('output/3043210_s4_totxt.txt', header=True, index=False)
+        file.to_csv('output/' + str(skaiciukas)+".txt", header=True, index=False)
 
-    os.remove('output/3043210_s3_merge.xlsx')
+    os.remove('output/' + str(skaiciukas)+".xlsx")
 
 
 def istrink_bruksniukus():
-    file = open('output/3043210_s4_totxt.txt')
+    file = open('output/' + str(skaiciukas)+".txt")
     contents = file.read()
     # print(contents)
     replaced_contents = contents.replace(',', '_')
     # print(replaced_contents)
 
-    with open('output/3043210_s5_find_replace.txt', "w") as f:
+    with open('output/' + str(skaiciukas)+".txt", "w") as f:
         for i in replaced_contents:
             f.write(i)
-
-    os.remove('output/3043210_s4_totxt.txt')            
 
 
 def delete_trash():
     # Karina
     # https://www.csnewbs.com/python-10c-remove-edit-lines
 
-    with open("output/3043210_s5_find_replace.txt", "r") as file:
+    with open('output/' + str(skaiciukas)+".txt", 'r') as file:
         lines = file.readlines()
-    with open('sourcefiles/3043210_siuksles.txt', 'r') as file:
+    with open('sourcefiles/' + str(skaiciukas)+"_siuksles.txt", 'r') as file:
         to_delete = file.read().splitlines()
 
-    file = open("output/3043210_s5_find_replace.txt", "w")
+    file = open('output/' + str(skaiciukas)+".txt", 'w')
     for line in lines:
         if line.rstrip() not in to_delete:
             file.write(line)
             # print(line, "liko po pravalymo")
     file.close()
 
-    os.rename('output/3043210_s5_find_replace.txt', 'output/3043210.txt')
+delete_cols_rows()
+no_sca()
+merge()
+convert_to_txt()
+istrink_bruksniukus()
+delete_trash()
